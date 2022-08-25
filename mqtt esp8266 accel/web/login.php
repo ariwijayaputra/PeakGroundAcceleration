a9cbfe307a698e9064e376306bb290cd624bef31
ariwijayaputra <62342206+ariwijayaputra@users.noreply.github.com>
2022-08-25 20:51:01 +0800

    penambahan tittle dan animation pada login

--- mqtt esp8266 accel/web/login.php
+++ mqtt esp8266 accel/web/login.php
@@ -9,15 +9,33 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400&display=swap" rel="stylesheet"></head>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+    
 <body style="
     background-image: url('./blueAbstract.jpg'); 
     height: 100%; 
     background-position: center;
     background-repeat: no-repeat;
     background-size: cover;">
-    <div class="d-flex justify-content-center container-fluid h-100" >
-        <div class="col-lg-6 my-auto mx-auto">
+    <div class="d-flex row justify-content-center container-fluid h-100" >
+        <div class="col-lg-6 my-auto mx-auto text-white">
+            <div class = "row w-75 mx-auto">
+                <div class="col-lg-2"></div>
+                <div class="col-lg-10">
+                    <h1 class= "logintext1 ">Peak Ground</h1>
+                </div>
+            </div>
+            <div class = "row w-75 mx-auto">
+                <div class="col-lg-4"></div>
+                <div class="col-lg-8">
+                    <h1 class= "logintext2 ">Accelerometer</h1>
+                </div>
+
+            </div>
+        </div>
+        <div class="col-lg-6 my-auto mx-auto" id = 'loginbox'>
             <div class="card w-75 mx-auto border-0">
                 <div class="card-header">
                     <h3 class="py-0 text-center mb-0">Log in</h3>
